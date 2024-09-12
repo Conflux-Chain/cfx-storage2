311bcedb42b9bc2d79f01df0d80c35788e16f5b8
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-09-12 11:36:35 +0800

    cargo fmt

--- src/middlewares/versioned_flat_key_value/pending_part/pending_schema.rs
+++ src/middlewares/versioned_flat_key_value/pending_part/pending_schema.rs
@@ -17,39 +17,13 @@ pub type Key<S> = <S as PendingKeyValueSchema>::Key;
 pub type Value<S> = <S as PendingKeyValueSchema>::Value;
 pub type CommitId<S> = <S as PendingKeyValueSchema>::CommitId;
 
-pub type Modifications<S> = Vec<(
-    Key<S>,
-    Option<Value<S>>,
-    Option<CommitId<S>>,
-)>;
-pub type Commits<S> = HashMap<
-    Key<S>,
-    (
-        CommitId<S>,
-        Option<Value<S>>,
-    ),
->;
-pub type Rollbacks<S> =
-    HashMap<Key<S>, Option<CommitId<S>>>;
-pub type History<S> = HashMap<
-    CommitId<S>,
-    HashMap<Key<S>, Option<Value<S>>>,
->;
-pub type Current<S> = BTreeMap<
-    Key<S>,
-    (
-        CommitId<S>,
-        Option<Value<S>>,
-    ),
->;
-pub type ToCommit<S> = Vec<(
-    CommitId<S>,
-    Option<HashMap<Key<S>, Option<Value<S>>>>,
-)>;
-pub type CIdVecPair<S> = (
-    Vec<CommitId<S>>,
-    Vec<CommitId<S>>,
-);
+pub type Modifications<S> = Vec<(Key<S>, Option<Value<S>>, Option<CommitId<S>>)>;
+pub type Commits<S> = HashMap<Key<S>, (CommitId<S>, Option<Value<S>>)>;
+pub type Rollbacks<S> = HashMap<Key<S>, Option<CommitId<S>>>;
+pub type History<S> = HashMap<CommitId<S>, HashMap<Key<S>, Option<Value<S>>>>;
+pub type Current<S> = BTreeMap<Key<S>, (CommitId<S>, Option<Value<S>>)>;
+pub type ToCommit<S> = Vec<(CommitId<S>, Option<HashMap<Key<S>, Option<Value<S>>>>)>;
+pub type CIdVecPair<S> = (Vec<CommitId<S>>, Vec<CommitId<S>>);
 pub type RollComm<S> = (Rollbacks<S>, Commits<S>);
 
 pub struct PendingKeyValueConfig<T, CId> {
