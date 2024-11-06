67b8158d5511b1a90b3d979641871883be4bb46c
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-11-06 16:29:06 +0800

    change_root bfs to_remove instead of to_retain

--- src/middlewares/versioned_flat_key_value/pending_part/tree/change_root.rs
+++ src/middlewares/versioned_flat_key_value/pending_part/tree/change_root.rs
@@ -1,4 +1,4 @@
-use std::collections::{BTreeSet, VecDeque};
+use std::collections::VecDeque;
 
 use crate::middlewares::versioned_flat_key_value::pending_part::pending_schema::{
     KeyValueMap, PendingKeyValueSchema, Result as PendResult,
@@ -18,24 +18,22 @@ impl<S: PendingKeyValueSchema> Tree<S> {
         // old_root..=new_root's parent
         let to_commit = self.find_path(slab_index);
 
-        let parent_of_new_root = if let Some(last) = to_commit.last() {
-            last.0
-        } else {
-            // early return if new_root == old_root
+        if let Some(last) = to_commit.last() {
+            for (ancester, _) in to_commit.iter() {
+                self.discard(*ancester)?;
+            }
+            self.discard(commit_id)?;
 
-            assert_eq!(self.height_of_root, self.nodes[slab_index].get_height());
-            return Ok((self.height_of_root, to_commit));
-        };
+            for (ancester, _) in to_commit.iter() {
+                self.detach_node(self.get_slab_index_by_commit_id(*ancester).unwrap())
+            }
 
-        for idx in self.find_remove_node_index(slab_index) {
-            self.detach_node(idx);
+            // set new_root as root
+            let new_root = self.get_node_mut_by_slab_index(slab_index);
+            new_root.set_as_root();
+            self.height_of_root = new_root.get_height();
+            self.parent_of_root = Some(last.0);
         }
-
-        // set new_root as root
-        let new_root = self.get_node_mut_by_slab_index(slab_index);
-        new_root.set_as_root();
-        self.height_of_root = new_root.get_height();
-        self.parent_of_root = Some(parent_of_new_root);
 
         // (height of old_root, old_root..=new_root's parent)
         Ok((self.height_of_root - to_commit.len(), to_commit))
@@ -50,20 +48,5 @@ impl<S: PendingKeyValueSchema> Tree<S> {
             path.push_front((target_node.get_commit_id(), target_node.get_updates()));
         }
         path.into()
-    }
-
-    fn find_remove_node_index(&self, retain_subtree_root: usize) -> impl Iterator<Item = usize> {
-        // subtree of new_root
-        let to_maintain_vec = self.bfs_subtree(retain_subtree_root);
-        let to_maintain = BTreeSet::from_iter(to_maintain_vec);
-
-        // remove: tree - subtree of new_root
-        let mut to_remove = Vec::new();
-        for (idx, _) in self.nodes.iter() {
-            if !to_maintain.contains(&idx) {
-                to_remove.push(idx);
-            }
-        }
-        to_remove.into_iter()
     }
 }
