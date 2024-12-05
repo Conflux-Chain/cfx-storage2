d13b1ccd4c3d212db1d4696f4a9c1ffeeb7ce4cd
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-12-05 15:28:52 +0800

    get_pp_from_ppot supports inputted input_type

--- src/bin/get_pp_from_ppot.rs
+++ src/bin/get_pp_from_ppot.rs
@@ -3,12 +3,12 @@ use cfx_storage2::{load_save_power_tau, InputType};
 use tracing::Level;
 use tracing_subscriber::fmt::format::FmtSpan;
 
-fn parse_param() -> Result<(usize, usize, String, String)> {
+fn parse_param() -> Result<(InputType, usize, usize, String, String)> {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() < 5 {
+    if args.len() < 6 {
         bail!(
-            "Usage: {} <challenge_path> <file_size_pow> <target_size_pow> <output_path>",
+            "Usage: {} <challenge_path> <file_size_pow> <target_size_pow> <output_path> <input_type>",
             args[0]
         );
     }
@@ -20,13 +20,32 @@ fn parse_param() -> Result<(usize, usize, String, String)> {
 
     if file_size_pow < read_size_pow {
         bail!(
-            "Usage: {} <challenge_path> <file_size_pow> <target_size_pow> <output_path>\n
+            "Usage: {} <challenge_path> <file_size_pow> <target_size_pow> <output_path> <input_type>\n
             <file_size_pow> should be the largest, 
             <target_size_pow> should be the smallest",
             args[0]
         );
     }
-    Ok((file_size_pow, read_size_pow, challenge_path, output_path))
+
+    let input_type = &args[5];
+    let input_type = if input_type == "Challenge" {
+        InputType::Challenge
+    } else if input_type == "Response" {
+        InputType::Response
+    } else {
+        bail!(
+            "Usage: {} <challenge_path> <file_size_pow> <target_size_pow> <output_path> <input_type>\n
+            <input_type> should be either 'Challenge' or 'Response'.",
+            args[0]
+        );
+    };
+    Ok((
+        input_type,
+        file_size_pow,
+        read_size_pow,
+        challenge_path,
+        output_path,
+    ))
 }
 
 fn main() {
@@ -36,17 +55,17 @@ fn main() {
         .with_target(false)
         .init();
 
-    let (file_size_pow, target_size_pow, challenge_path, output_path) = match parse_param() {
-        Ok(x) => x,
-        Err(e) => {
-            eprintln!("Cannot parse input: {:?}", e);
-            std::process::exit(1);
-        }
-    };
+    let (input_type, file_size_pow, target_size_pow, challenge_path, output_path) =
+        match parse_param() {
+            Ok(x) => x,
+            Err(e) => {
+                eprintln!("Cannot parse input: {:?}", e);
+                std::process::exit(1);
+            }
+        };
 
     let input_path = challenge_path;
-    // let input_type = InputType::Challenge;
-    let input_type = InputType::Response;
+
     let chunk_size_pow = std::cmp::min(target_size_pow, 16);
     let dir = &output_path;
     load_save_power_tau(
