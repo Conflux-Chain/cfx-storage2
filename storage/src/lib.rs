ac290f32dd1ae043e12ffa021cd034ffd6a00ad8
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-12-18 16:18:00 +0800

    separate amt&ppot2ark to another repo

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -1,8 +1,5 @@
 #![allow(dead_code, unused_variables)]
 
-#[macro_use]
-extern crate tracing;
-
 pub mod backends;
 pub mod errors;
 mod example;
@@ -14,7 +11,6 @@ pub mod types;
 mod utils;
 
 pub use errors::{Result, StorageError};
-pub use lvmt::{ptau_file_name, AmtParams, CreateMode, PowerTau};
 
 #[derive(Default)]
 struct DummpyIter<T>(std::marker::PhantomData<T>);
