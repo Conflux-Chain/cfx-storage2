8ca3a4a42b42c794281208f107e6bb80f508d818
Rong Ma <160457111+rongma7@users.noreply.github.com>
2024-12-31 18:28:48 +0800

    Add a constructor function to `LvmtStore` and include an example for LVMT

---
+++ src/lvmt/example.rs
@@ -0,0 +1,50 @@
+use std::sync::Arc;
+
+use crate::{
+    backends::DatabaseTrait,
+    errors::Result,
+    middlewares::{KeyValueStoreBulks, VersionedStore, VersionedStoreCache},
+};
+
+use super::{
+    auth_changes::AuthChangeTable,
+    storage::LvmtStore,
+    table_schema::{AmtNodes, FlatKeyValue, SlotAllocations},
+};
+
+pub struct LvmtStorage<D: DatabaseTrait> {
+    backend: D,
+    key_value_cache: VersionedStoreCache<FlatKeyValue>,
+    amt_node_cache: VersionedStoreCache<AmtNodes>,
+    slot_alloc_cache: VersionedStoreCache<SlotAllocations>,
+}
+
+impl<D: DatabaseTrait> LvmtStorage<D> {
+    pub fn new(backend: D) -> Result<Self> {
+        Ok(Self {
+            backend,
+            key_value_cache: VersionedStoreCache::new_empty(),
+            amt_node_cache: VersionedStoreCache::new_empty(),
+            slot_alloc_cache: VersionedStoreCache::new_empty(),
+        })
+    }
+
+    pub fn as_manager(&mut self) -> Result<LvmtStore<'_, '_>> {
+        let key_value_store = VersionedStore::new(&self.backend, &mut self.key_value_cache)?;
+        let amt_node_store = VersionedStore::new(&self.backend, &mut self.amt_node_cache)?;
+        let slot_alloc_store = VersionedStore::new(&self.backend, &mut self.slot_alloc_cache)?;
+        let auth_changes =
+            KeyValueStoreBulks::new(Arc::new(self.backend.view::<AuthChangeTable>()?));
+
+        Ok(LvmtStore::new(
+            key_value_store,
+            amt_node_store,
+            slot_alloc_store,
+            auth_changes,
+        ))
+    }
+
+    pub fn commit_auth(&mut self, write_schema: <D as DatabaseTrait>::WriteSchema) -> Result<()> {
+        self.backend.commit(write_schema)
+    }
+}
